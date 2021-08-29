6ed1fb649691065b9b3cdb58bfb9c2f410c9e6fd
emmanuel Harry <emma4lil@gmail.com>
2021-08-29 23:48:50 +0100

    beta

--- pages/flyers/index.vue
+++ pages/flyers/index.vue
@@ -1,32 +1,116 @@
 <template>
   <v-row>
-    <v-col class="" cols="12">
+    <v-col cols="12" lg="6">
       <v-card>
-        <v-card-title>
-          <div class="text-h2">Flyers</div>
-          <v-spacer></v-spacer>
-          <v-text-field
-            v-model="search"
-            append-icon="mdi-magnify"
-            label="Search"
-            single-line
-            hide-details
-          ></v-text-field>
-        </v-card-title>
-        <v-data-table
-          class="mt-8"
-          :headers="headers"
-          :items="flyers"
-          :search="search"
-          @click:row="rowClicked"
-        ></v-data-table>
+        <v-card-text>
+          <v-row>
+            <v-col class="" cols="12">
+              <v-card>
+                <v-card-title>
+                  <div class="text-h3 grey--text">Flyers</div>
+                  <v-spacer></v-spacer>
+                  <v-text-field
+                    v-model="search"
+                    append-icon="mdi-magnify"
+                    label="Search by column"
+                    single-line
+                    hide-details
+                  ></v-text-field>
+                </v-card-title>
+                <v-data-table
+                  dense
+                  class="mt-8"
+                  :headers="headers"
+                  :items="flyers"
+                  :search="search"
+                  @click:row="rowClicked"
+                ></v-data-table>
+              </v-card>
+            </v-col>
+          </v-row>
+        </v-card-text>
+        <v-card-actions>
+          <v-spacer />
+          <v-btn small color="primary" dark>See Trades</v-btn>
+          <v-btn small color="green" dark>Unpublish</v-btn>
+          <v-btn small color="red" dark>Publish</v-btn>
+          <v-btn small dark>Delete</v-btn>
+        </v-card-actions>
       </v-card>
     </v-col>
-    <v-col class="blue" cols="6">
+    <v-col cols="12" lg="6">
+      <v-row> 
+        <v-col cols="">
+          <v-carousel hide-delimiter-background hide-delimiters>
+            <v-carousel-item
+              v-for="(item, i) in items"
+              :key="i"
+              :src="item.src"
+              reverse-transition="fade-transition"
+              transition="fade-transition"
+            ></v-carousel-item>
+          </v-carousel>
+        </v-col>
+        <v-col cols="">
+          <!-- Simple Table -->
+          <h3>Desription</h3>
+          <v-divider></v-divider>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel
+            distinctio aperiam illo mollitia excepturi repellat ratione, nobis
+            neque quae eligendi culpa consequatur similique ducimus, quidem
+            nostrum, nulla suscipit sint tenetur?
+          </p>
+          <h3>Location and Gps</h3>
+          <v-divider></v-divider>
+          <div class="caption">
+            <div class="d-flex justify-space-between">
+              <strong>lat:</strong>
+              <div>3.4.4433</div>
+            </div>
+            <div class="d-flex justify-space-between">
+              <strong>long:</strong>
+              <div>3.4.4433</div>
+            </div>
+            <div class="d-flex justify-space-between">
+              <strong>Country:</strong>
+              <div>Nigeria</div>
+            </div>
+            <div class="d-flex justify-space-between">
+              <strong>State:</strong>
+              <div>Rivers</div>
+            </div>
+            <div class="d-flex justify-space-between">
+              <strong>Province: </strong>
+              <div>Port Harcourt</div>
+            </div>
+          </div>
+          <!-- Owner Info -->
+          <h3 class="mt-2">Ownership Information</h3>
+          <v-divider></v-divider>
+          <div class="d-flex justify-space-between">
+            <strong>Posted by: </strong>
+            <div>Emmanuel harry</div>
+          </div>
+          <div class="d-flex justify-space-between">
+            <strong>Email: </strong>
+            <div>emma4lil@gmail.com</div>
+          </div>
+          <div class="d-flex justify-space-between">
+            <strong>joined: </strong>
+            <div>2042-08-09</div>
+          </div>
+
+          {{ info }}
+        </v-col>
+      </v-row>
+    </v-col>
+
+    <!-- <v-col class="blue" cols="6">
       <div>flyers actions and controls</div>
       <div>{{ flyers }}</div>
-      <div>{{info}}</div>
-    </v-col>
+      <div>{{ info }}</div>
+    </v-col> -->
   </v-row>
 </template>
 
@@ -47,96 +131,27 @@ export default {
         { text: "Email", value: "owner.email" },
         { text: "TradeCount", value: "tradeCount" },
         { text: "Description", value: "description" },
-      
       ],
-      desserts: [
-        {
-          name: "Frozen Yogurt",
-          calories: 159,
-          fat: 6.0,
-          carbs: 24,
-          protein: 4.0,
-          iron: "1%",
-        },
-        {
-          name: "Ice cream sandwich",
-          calories: 237,
-          fat: 9.0,
-          carbs: 37,
-          protein: 4.3,
-          iron: "1%",
-        },
-        {
-          name: "Eclair",
-          calories: 262,
-          fat: 16.0,
-          carbs: 23,
-          protein: 6.0,
-          iron: "7%",
-        },
-        {
-          name: "Cupcake",
-          calories: 305,
-          fat: 3.7,
-          carbs: 67,
-          protein: 4.3,
-          iron: "8%",
-        },
-        {
-          name: "Gingerbread",
-          calories: 356,
-          fat: 16.0,
-          carbs: 49,
-          protein: 3.9,
-          iron: "16%",
-        },
-        {
-          name: "Jelly bean",
-          calories: 375,
-          fat: 0.0,
-          carbs: 94,
-          protein: 0.0,
-          iron: "0%",
-        },
-        {
-          name: "Lollipop",
-          calories: 392,
-          fat: 0.2,
-          carbs: 98,
-          protein: 0,
-          iron: "2%",
-        },
-        {
-          name: "Honeycomb",
-          calories: 408,
-          fat: 3.2,
-          carbs: 87,
-          protein: 6.5,
-          iron: "45%",
-        },
-        {
-          name: "Donut",
-          calories: 452,
-          fat: 25.0,
-          carbs: 51,
-          protein: 4.9,
-          iron: "22%",
-        },
-        {
-          name: "KitKat",
-          calories: 518,
-          fat: 26.0,
-          carbs: 65,
-          protein: 7,
-          iron: "6%",
-        },
-      ],
-      info: "info",
+      info: [],
       flyers: [],
+      items: [
+        {
+          src: "https://cdn.vuetifyjs.com/images/carousel/squirrel.jpg",
+        },
+        {
+          src: "https://cdn.vuetifyjs.com/images/carousel/sky.jpg",
+        },
+        {
+          src: "https://cdn.vuetifyjs.com/images/carousel/bird.jpg",
+        },
+        {
+          src: "https://cdn.vuetifyjs.com/images/carousel/planet.jpg",
+        },
+      ],
     };
   },
-  mounted () {
-      this.getFlyers()
+  mounted() {
+    this.getFlyers();
   },
   methods: {
     rowClicked(arg1, arg2) {
@@ -147,6 +162,9 @@ export default {
         this.flyers = d.data;
       });
     },
+    toggleflyerAction() {},
+    seeTradeAction() {},
+    deleteAction() {},
   },
 };
 </script>
