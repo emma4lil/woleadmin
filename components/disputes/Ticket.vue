8daed788d69130028b13c1ee6622e3785599655b
EmmanuelHarry <eharry@axxess.com>
2022-07-20 21:28:40 +0100

    boom

--- components/disputes/Ticket.vue
+++ components/disputes/Ticket.vue
@@ -1,33 +1,32 @@
 <template>
   <v-card>
-    <div
-      class="
+    <div class="
         pa-2
         text-caption
         grey--text
         lighten-3
         d-flex
-        justify-space-between
-      "
-    >
-      <div>Ticket#{{ index + 1 }}</div>
-      <div class="blue--text" v-if="ticket.moderator">{{ticket.moderator}}</div>
-      <div v-else>No Moderator</div>
+        flex-column
+      ">
+
+      <div>T{{index}}#{{ ticket.id.substring(-1,5) }} - <v-chip x-small>{{ ticket.isResolved ? "Resolved" : "Not Resolved" }}</v-chip></div>
+      <div class="blue--text" v-if="ticket.moderator">{{ ticket.moderator }}</div>
+      <div v-else class="orange--text">No Moderator</div>
+    </div>
+    <div class="mx-2 grey--text text-caption">
+      Created: {{ new Date(ticket.created).toLocaleString() }}
     </div>
     <v-divider></v-divider>
     <div class="d-flex flex-column text-center font-weight-light">
       <div>{{ ticket.flyerOwner }} - owner</div>
       <div>vs</div>
       <div>
-        {{ ticket.consumer }} - <span v-if="ticket.whoIsPaying">buyer</span
-        ><span v-if="!ticket.whoIsPaying">seller</span>
+        {{ ticket.consumer }} - <span v-if="ticket.whoIsPaying">buyer</span><span
+          v-if="!ticket.whoIsPaying">seller</span>
       </div>
     </div>
-
     <v-card-actions>
-      <v-btn @click="gotoTrades()" class="elevation-0" x-small color="success"
-        >see trade</v-btn
-      >
+      <v-btn @click="gotoTrades()" class="elevation-0" x-small color="success">see trade</v-btn>
       <v-spacer></v-spacer>
       <v-btn @click="setFocus()" class="elevation-0" x-small color="blue">Moderate</v-btn>
     </v-card-actions>
@@ -45,7 +44,7 @@ export default {
     gotoTrades() {
       this.$router.push("/trades/" + this.ticket.tradeId);
     },
-    setModrator(){
+    setModrator() {
 
     }
   },
